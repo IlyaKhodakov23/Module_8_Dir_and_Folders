dc436e3045cbedd7eaab4ce8c1465fa7d72eaad5
Ilya <ilyakhodakov23@gmail.com>
2022-12-06 23:53:34 +0300

    Расчет размеров файлов добавил

--- Task_2_Module_8/Task_2_Module_8/Program.cs
+++ Task_2_Module_8/Task_2_Module_8/Program.cs
@@ -5,7 +5,36 @@
         static string dirName = @"C:\Users\user\Desktop\IT\OldDir";
         static void Main(string[] args)
         {
-            
+            long size = CheckSize(dirName);
+            Console.WriteLine(size);
+        }
+        static long CheckSize(string dir)
+        {
+            long size = 0;
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dir);
+                FileInfo[] files = dirInfo.GetFiles();
+                DirectoryInfo[] dirs = dirInfo.GetDirectories();
+                if (dirInfo.Exists)
+                {
+                    foreach (FileInfo file in files)
+                    {
+                        Console.WriteLine(file.FullName);
+                        size = size + file.Length;
+                    }
+
+                    foreach (DirectoryInfo directoryInfo in dirs)
+                    {
+                        CheckSize(directoryInfo.FullName);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return size;
         }
     }
 }
