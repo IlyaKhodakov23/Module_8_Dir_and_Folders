9dec193021186f115b8ba575c46221a3c3820b5c
Ilya <ilyakhodakov23@gmail.com>
2022-12-07 21:02:51 +0300

    Добавил описание задачи

--- Task1_Module_8/Task1_Module_8/Program.cs
+++ Task1_Module_8/Task1_Module_8/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        //Напишите программу, которая чистит нужную нам папку от файлов  и папок, которые не использовались более 30 минут 
         static string dirName = @"C:\Users\user\Desktop\IT\OldDir";
         static void Main(string[] args)
         {
---
+++ Task_3_Module_8/Task_3_Module_8/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task_3_Module_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
